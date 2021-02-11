c046aaa22fab8bb5bb9951be8c8e66c7e30d71e3
Sean Z <contact@seanzeng.com>
2021-02-11 23:39:31 +1300

    v0.1.1 - add version

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import {
+    Box,
     Container,
     createStyles,
     Grid,
@@ -8,18 +9,21 @@ import {
 import React from 'react'
 
 import { TypingMain } from '../components/typing/TypingMain'
+import { VERSION } from '../constants'
 
 const styles = makeStyles((theme) =>
     createStyles({
         mainContainer: {
             paddingTop: theme.spacing(5),
-            display: 'flex',
         },
         typingContainer: {
             width: '100%',
             [theme.breakpoints.up('md')]: {
                 width: '65%',
             },
+        },
+        subtitle: {
+            paddingLeft: theme.spacing(1),
         },
     })
 )
@@ -30,8 +34,26 @@ export const MainPage: React.FC = () => {
     return (
         <Container className={classes.mainContainer}>
             <Grid container spacing={2} justify="center">
-                <Grid item container justify="center" xs={12}>
-                    <Typography variant={'h3'}>PathToLife Typing</Typography>
+                <Grid item container xs={12} alignItems={'flex-end'}>
+                    <Box fontStyle={'italic'}>
+                        <Typography display={'inline'} variant={'h4'}>
+                            PathToLife
+                        </Typography>
+                    </Box>
+                    <Typography
+                        display={'inline'}
+                        variant={'h4'}
+                        className={classes.subtitle}
+                    >
+                        Typing
+                    </Typography>
+                    <Typography
+                        display={'inline'}
+                        variant={'subtitle1'}
+                        className={classes.subtitle}
+                    >
+                        v{VERSION}
+                    </Typography>
                 </Grid>
                 <Grid item container justify="center" xs={12}>
                     <div className={classes.typingContainer}>
